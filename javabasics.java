d0ddd45c0fb0f629ff9a144164607c5353aab26c
D L Hemalatha <hemadlhemalatha@gmail.com>
2026-05-25 21:07:08 +0530

    javabasics.java

--- javabasics.java
+++ javabasics.java
@@ -18,4 +18,24 @@ public class variables {
     }
 }
 //data types
+public class datatypes {
+    public static void main(String[] args) {
+        int number=2;
+        float price=10.5f;
+        char ch='a';
+        boolean var=true;
+        byte b=8;
+        //long
+        //float
+        short n=220;
+        System.out.println(number);
+        System.out.println(price);
+        System.out.println(ch);
+        System.out.println(var);
+        System.out.println(b);
+        System.out.println(n);
+     }
+    
+}
+
 
