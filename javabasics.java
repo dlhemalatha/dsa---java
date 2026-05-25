20a6803af430b7419e0c61ef317f16a4414109ab
D L Hemalatha <hemadlhemalatha@gmail.com>
2026-05-25 21:03:27 +0530

    javabasics,variables,Data types

--- javabasics.java
+++ javabasics.java
@@ -5,3 +5,17 @@ public class javabasics {
     }
     
 }
+//variables
+public class variables {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        String name= "tony shark";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+    }
+}
+//data types
+
